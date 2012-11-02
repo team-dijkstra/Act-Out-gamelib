794e45dfc495b6d0f06ca8aca8c6b787f6e0955d
cpinter10 <cpinter10@gmail.com>
2012-11-02 12:13:22 -0600

    added virtual keyword to unit.h

--- unit.h
+++ unit.h
@@ -3,13 +3,13 @@
 
 class Unit {
    //accessors
-   int increase (Unit u) =0;
-   int decrease (Unit u) =0;
+   virtual int increase (Unit u) =0;
+   virtual int decrease (Unit u) =0;
 
    //mutators
-   int numUnits() =0;
-   Territory whereAt() =0;
-   String name() =0;
-   List<Action> actions =0;
+   virtual int numUnits() =0;
+   virtual Territory whereAt() =0;
+   virtual String name() =0;
+   virtual List<Action> actions =0;
    
 };
