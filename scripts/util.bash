dbfb857a71650c539c136160d8bd4ce69308981c
Matt Voroney <matt.voroney@gmail.com>
2012-11-10 21:05:38 -0700

    [skip ci] fixes #45. closes #44. fixed license check, and added script to add licences to the tops of files.

--- scripts/util.bash
+++ scripts/util.bash
@@ -1,4 +1,9 @@
 
+#
+# Quotes regular expression metacharacters. Outputs the transformed string.
+#
+# \param line the line to quote.
+#
 function quote_meta {
     echo $1 | sed 's/\\/\\\\/g; s/\//\\\//g'
 }
@@ -17,17 +22,48 @@ function contains-block {
     local blockf=$2;
     shift 2
 
-    local block_start=$(quote_meta "`head -n 1 $blockf`")
-    local block_end=$(quote_meta "`tail -n 1 $blockf`")
+    local block_start=`head -n 1 $blockf`
+    local block_end=`tail -n 1 $blockf`
 
     local t=$(tempfile)
 
-    sed -n "/$block_start/,/$block_end/p" $file > $t
+    sed -n "\@$block_start@,\@$block_end@ p" $file > $t
     diff -q $t $blockf > /dev/null 2>&1
     local status=$?
     
     rm -f -- $t
 
     return $status
+}
+
+#
+# Prepends the contents of one file to another, encapsulated by pre and post.
+#
+# \param file the name of the file to modify.
+# \param pre the pre text to insert immediately before the text block.
+# \param block the name of the file containing the block to prepend.
+# \param post the post text to insert immediately following the text block.
+#
+function prepend-block {
+    local file=$1
+    local pre=$2
+    local blockf=$3
+    local post=$4
+    shift 4
+
+    contains-block $file $blockf
+    if [ $? -ne 0 ]; then
+        local t=$(tempfile)
+       
+        # build the new file
+        echo "$pre" > $t
+        cat $blockf >> $t
+        echo "$post" >> $t
+        cat $file >> $t
+
+        # swap with original
+        mv -f $t $file
+    fi
+
 }
 
