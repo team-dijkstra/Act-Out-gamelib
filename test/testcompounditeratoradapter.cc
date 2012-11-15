3b4fdae2d8faa63a408a6a034e668ccecc590d56
Matt Voroney <matt.voroney@gmail.com>
2012-11-14 23:30:00 -0700

    addresses #13. parked iterator for now. type mismatch errors too tricky to sort out right now.

--- test/testcompounditeratoradapter.cc
+++ test/testcompounditeratoradapter.cc
@@ -63,7 +63,7 @@ class TestCompoundIteratorAdapter : public CppUnit::TestFixture {
    void tearDown() {
       tc.clear();
    }
-   /// \endcondThis file is part 
+   /// \endcond
 
    /// \test ensure that a == a is true for the adapter.
    void iterator_should_be_equal_to_self() {
@@ -116,6 +116,7 @@ class TestCompoundIteratorAdapter : public CppUnit::TestFixture {
          ci2++;
       }
    }
+   
 
 };
 
