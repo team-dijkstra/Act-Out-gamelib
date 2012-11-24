deb346373d10f1df41479b86408d9601ced98ef1
Matt Voroney <matt.voroney@gmail.com>
2012-11-24 16:14:23 -0700

    [ci skip] addresses #71. completed refactoring. formatted include/namecomparators.h.

--- include/namecomparators.h
+++ include/namecomparators.h
@@ -17,84 +17,84 @@
  */
 namespace compare {
 
-/**
- * \namespace compare::byname
- *
- * Supplies comparison methods that operate on a 'name()' member.
- * 
- * \todo it should be possible to reuse some of these operator definitions
- *    since they are given purely in terms of < and ==.
- */
-namespace byname {
+   /**
+    * \namespace compare::byname
+    *
+    * Supplies comparison methods that operate on a 'name()' member.
+    * 
+    * \todo it should be possible to reuse some of these operator definitions
+    *    since they are given purely in terms of < and ==.
+    */
+   namespace byname {
 
-/**
- * Less than operator to compare objects by name.
- *
- * \tparam T the type of the objects to compare. T must supply a 'name()' 
- *    method that takes no arguments and returns a std::string.
- *
- * \param lhs The left hand side operand.
- * \param rhs The right hand side operand.
- *
- * \return true if \begincode lhs.name() < rhs.name() \endcode. false otherwise.
- */
-template<typename T>
-bool operator< (const T & lhs, const T & rhs) {
-   return (lhs.name() < rhs.name());
-}
+      /**
+       * Less than operator to compare objects by name.
+       *
+       * \tparam T the type of the objects to compare. T must supply a 'name()' 
+       *    method that takes no arguments and returns a std::string.
+       *
+       * \param lhs The left hand side operand.
+       * \param rhs The right hand side operand.
+       *
+       * \return true if \begincode lhs.name() < rhs.name() \endcode. false otherwise.
+       */
+      template<typename T>
+      bool operator< (const T & lhs, const T & rhs) {
+         return (lhs.name() < rhs.name());
+      }
 
-/**
- * Equality operator to compare objects by name.
- *
- * \tparam T the type of the objects to compare. T must supply a 'name()'
- *    method that takes no arguments and returns a std::string.
- *
- * \param lhs The left hand side operand.
- * \param rhs The right hand side operand.
- *
- * \return true if \begincode lhs.name() == rhs.name() \endcode. false
- *    otherwise.
- */
-template<typename T>
-bool operator== (const T & lhs, const T & rhs) {
-   return (lhs.name() == rhs.name());
-}
+      /**
+       * Equality operator to compare objects by name.
+       *
+       * \tparam T the type of the objects to compare. T must supply a 'name()'
+       *    method that takes no arguments and returns a std::string.
+       *
+       * \param lhs The left hand side operand.
+       * \param rhs The right hand side operand.
+       *
+       * \return true if \begincode lhs.name() == rhs.name() \endcode. false
+       *    otherwise.
+       */
+      template<typename T>
+      bool operator== (const T & lhs, const T & rhs) {
+         return (lhs.name() == rhs.name());
+      }
 
-/**
- * Less than or equal comparison operator. Implemented in terms of 
- * operator< and operator==.
- * 
- * \see operator<
- * \see operator==
- */
-template<typename T>
-bool operator<= (const T & lhs, const T & rhs) {
-   return ((lhs < rhs) || (lhs == rhs));
-}
-/**
- * Greater than comparison operator. Implemented in terms of 
- * operator< and operator==.
- * 
- * \see operator<
- * \see operator==
- */
-template<typename T>
-bool operator> (const T & lhs, const T & rhs) {
-   return !(lhs <= rhs);
-}
+      /**
+       * Less than or equal comparison operator. Implemented in terms of 
+       * operator< and operator==.
+       * 
+       * \see operator<
+       * \see operator==
+       */
+      template<typename T>
+      bool operator<= (const T & lhs, const T & rhs) {
+         return ((lhs < rhs) || (lhs == rhs));
+      }
+      /**
+       * Greater than comparison operator. Implemented in terms of 
+       * operator< and operator==.
+       * 
+       * \see operator<
+       * \see operator==
+       */
+      template<typename T>
+      bool operator> (const T & lhs, const T & rhs) {
+         return !(lhs <= rhs);
+      }
 
-/**
- * Greater than or equal comparison operator. Implemented in terms of 
- * operator< and operator==.
- * 
- * \see operator<
- * \see operator==
- */
-template<typename T>
-bool operator>= (const T & lhs, const T & rhs) {
-   return ((lhs == rhs) || (lhs > rhs));
-}
-}
+      /**
+       * Greater than or equal comparison operator. Implemented in terms of 
+       * operator< and operator==.
+       * 
+       * \see operator<
+       * \see operator==
+       */
+      template<typename T>
+      bool operator>= (const T & lhs, const T & rhs) {
+         return ((lhs == rhs) || (lhs > rhs));
+      }
+   }
 }
 
 #endif /* NAME_COMPARATORS_H */
